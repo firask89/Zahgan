c183c282bc420fa8bcd8796247d6bd177fd1fd5c
IssaAnagreh <isa_anagreh@hotmail.com>
2018-12-09 18:51:57 +0200

    <fix> home page rout bug

--- react-client/src/components/Home/HomeClass.js
+++ react-client/src/components/Home/HomeClass.js
@@ -1,56 +1,43 @@
 import React from 'react'
-import {BrowserRouter ,Route ,Switch} from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import EventClassNew from './EventClassNew'
 import Create from '../Creator/Create'
 import Slideshow from '../Slider/Slideshow';
 import Vision from '../about/Vision';
 
 class HomeClass extends React.Component {
-constructor(props){
-    super(props)
-    this.myRef = React.createRef();
+    constructor(props) {
+        super(props)
+        this.myRef = React.createRef();
+    }
 
-    
-    
-}
-
-
-
-scrollToMyRef = () => {
-    window.scrollTo({
-        top:this.myRef.current.offsetTop, 
-        behavior: "smooth"
-    })
-}
-render(){
-    console.log("HomeClass",this.props.items)
-    return(
-       
-  <div>
-			
-          
-			<div className="container-fluid " >
-         <Slideshow/>
-            <Vision/>
-      	<div classsName="images">
-          <p className="main_title"><span>Events</span></p>
-        {
- this.props.items.map((item) =>{
-   return(<div ref={this.myRef}>
-    <EventClassNew item={item}/>
-    </div>)
- 
-})
-
-        }  
-        
-				</div>
-				
-			</div>
-   
-              </div>
-    )
-}
+    scrollToMyRef = () => {
+        window.scrollTo({
+            top: this.myRef.current.offsetTop,
+            behavior: "smooth"
+        })
+    }
+    render() {
+        console.log("HomeClass", this.props.items)
+        return (
+            <div>
+                <div className="container-fluid " >
+                    <Slideshow />
+                    <Vision />
+                    <div classsName="images">
+                        <p className="main_title"><span>Events</span></p>
+                        {
+                            this.props.items.map((item) => {
+                                return (<div ref={this.myRef}>
+                                    <EventClassNew item={item} />
+                                </div>)
+                            })
+                        }
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default HomeClass
